85d698b4d35d0eaaf7347a3ed4f96be9da2835eb
steve00d <stevedaenen@hotmail.com>
2024-12-18 13:29:10 +0100

    intitial commit

---
+++ src/main/java/com/switchfully/parkshark_2024_10/ParkShark202410Application.java
@@ -0,0 +1,13 @@
+package com.switchfully.parkshark_2024_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkShark202410Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkShark202410Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/parkshark_2024_10/ParkShark202410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.parkshark_2024_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkShark202410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
