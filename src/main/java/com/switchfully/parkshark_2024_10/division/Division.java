f56e9132eb3b9c76cd0db1f571fed23b329eb3c2
steve00d <stevedaenen@hotmail.com>
2024-12-19 11:53:40 +0100

    ContactInformation

--- src/main/java/com/switchfully/parkshark_2024_10/division/Division.java
+++ src/main/java/com/switchfully/parkshark_2024_10/division/Division.java
@@ -1,13 +1,46 @@
 package com.switchfully.parkshark_2024_10.division;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Division {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "division_seq")
+    @SequenceGenerator(name = "division_seq", sequenceName = "division_seq", allocationSize = 1)
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private  Director director;
+
+    @Column(name = "original_company_name")
+    private String OriginalCompanyName;
+
+    public Division(String name, Director director, String originalCompanyName) {
+        this.name = name;
+        this.director = director;
+        OriginalCompanyName = originalCompanyName;
+    }
+
+    public Division() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public String getOriginalCompanyName() {
+        return OriginalCompanyName;
+    }
 
     public void setId(Long id) {
         this.id = id;
