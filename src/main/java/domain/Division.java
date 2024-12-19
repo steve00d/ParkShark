4899a48f24ac0ebfc38755043e3f90ee711be6b5
satikoshapy <satikoshapy@yahoo.com>
2024-12-19 10:12:48 +0100

    added directory entity

---
+++ src/main/java/domain/Division.java
@@ -0,0 +1,19 @@
+package domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Division {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
