3f2d6837266e20ef858041ad684192a28505c1f3
steve00d <stevedaenen@hotmail.com>
2024-12-19 11:39:08 +0100

    ContactInformation

--- src/main/resources/db/migration/V2__add_ContactInformation.sql
+++ src/main/resources/db/migration/V2__add_ContactInformation.sql
@@ -1,13 +1,15 @@
-alter table adress rename to address;
+alter table adress
+    rename to address;
 
 create SEQUENCE contact_information_seq start with 1 increment by 1;
 create table contact_information
-(id bigint primary key default nextval('contact_information_seq'),
-    first_name varchar(255),
-    last_name varchar(255),
-    primary_phone_number varchar(255) not null,
+(
+    id                     bigint primary key default nextval('contact_information_seq'),
+    first_name             varchar(255),
+    last_name              varchar(255),
+    primary_phone_number   varchar(255) not null,
     secondary_phone_number varchar(255),
-    email_address varchar(255) not null,
-    fk_address_id bigint,
-    FOREIGN KEY (fk_address_id) REFERENCES address(id)
+    email_address          varchar(255) not null,
+    fk_address_id          bigint,
+    FOREIGN KEY (fk_address_id) REFERENCES address (id)
 );
