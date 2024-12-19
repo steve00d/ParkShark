0b7b0f9fad963abce06e99568d65073f42680542
steve00d <stevedaenen@hotmail.com>
2024-12-19 10:05:34 +0100

    32-address-zipcode-entity-4sp

---
+++ src/main/java/com/switchfully/parkshark_2024_10/adress/Zip.java
@@ -0,0 +1,44 @@
+package com.switchfully.parkshark_2024_10.adress;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "zip_code")
+public class Zip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "zip_code_seq")
+    @SequenceGenerator(name = "zip_code_seq", sequenceName = "zip_code_seq", allocationSize = 1)
+    private Long id;
+    @Column(name = "code")
+    private String code;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "state")
+    private String state;
+
+    public Zip(String code, String city, String state) {
+        this.code = code;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Zip() {
+        // DESERIALIZATION
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
---
+++ src/main/resources/db/migration/V1__intitial_database.sql
@@ -0,0 +1,21 @@
+
+
+create sequence zip_seq start with 1 increment by 1;
+create table zip_code
+(
+    id       int primary key default nextval('zip_seq'),
+    zip_code varchar(255),
+    city     varchar(255),
+    country  varchar(255)
+)
+
+CREATE sequence address_seq start with 1 increment by 1;
+Create table adress
+(
+    id     int primary key default nextval('address_seq'),
+    street varchar(255),
+    number integer,
+    extra  varchar(255),
+    fk_zip_code integer,
+    Fk_zip_code int references zip_code(id)
+);
