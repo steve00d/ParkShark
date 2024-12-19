557be155267712a610e88493584d8d52eff90d1b
steve00d <stevedaenen@hotmail.com>
2024-12-19 10:20:55 +0100

    all id's as long & bigint in db

--- src/main/resources/db/migration/V1__intitial_database.sql
+++ src/main/resources/db/migration/V1__intitial_database.sql
@@ -3,7 +3,7 @@
 create sequence zip_seq start with 1 increment by 1;
 create table zip_code
 (
-    id       int primary key default nextval('zip_seq'),
+    id       bigint primary key default nextval('zip_seq'),
     zip_code varchar(255),
     city     varchar(255),
     country  varchar(255)
@@ -13,10 +13,10 @@ CREATE sequence address_seq start with 1 increment by 1;
 
 Create table adress
 (
-    id     int primary key default nextval('address_seq'),
+    id     bigint primary key default nextval('address_seq'),
     street varchar(255),
     number integer,
     extra  varchar(255),
-    fk_zip_code integer,
-    Fk_zip_code int references zip_code(id)
+    fk_zip_code bigint,
+    foreign key (fk_zip_code) references zip_code(id)
 );
