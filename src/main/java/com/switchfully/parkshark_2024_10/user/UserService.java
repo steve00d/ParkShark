95177cf36077e77a0b8fd3dc826a9fa23fc5847d
Jens Descamps <jens.descamps@liantis.be>
2024-12-24 10:56:46 +0100

    Fixed 500 when no user found + added check for encoded password

--- src/main/java/com/switchfully/parkshark_2024_10/user/UserService.java
+++ src/main/java/com/switchfully/parkshark_2024_10/user/UserService.java
@@ -1,14 +1,16 @@
 package com.switchfully.parkshark_2024_10.user;
 
 
+import com.switchfully.parkshark_2024_10.exceptions.UnauthorizedException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class UserService {
-
-
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -17,8 +19,12 @@ public class UserService {
 
 
     public Person authenticate(String username, String password) {
-        return findByCredentials(username, password);
-
+        return Optional.ofNullable(findByCredentials(username, password))
+                .or(() -> {
+                    String encoded = Base64.getEncoder().encodeToString(password.getBytes());
+                    return Optional.ofNullable(findByCredentials(username, encoded));
+                })
+                .orElseThrow(UnauthorizedException::new);
     }
 
     private Person findByCredentials(String email, String password) {
