46a01d142b031cfb9bbe64f557f35ef1948c8154
Jens Descamps <jens.descamps@liantis.be>
2024-12-24 09:58:03 +0100

    Reset change to division_table in favor of adding it to a new migration

--- src/main/resources/db/migration/V4__add_division_table.sql
+++ src/main/resources/db/migration/V4__add_division_table.sql
@@ -5,6 +5,6 @@ create table division
     id                    bigint default nextval('division_seq') primary key,
     name                  varchar(255) not null,
     original_company_name varchar(255),
-    fk_director_id        bigint,
+    fk_director_id        bigint not null,
     foreign key (fk_director_id) references "user" (id)
 );
