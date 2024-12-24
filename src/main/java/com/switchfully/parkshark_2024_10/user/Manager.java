5c7861fee65ffb92d920fc6feabe586aea3b8350
Jens Descamps <jens.descamps@liantis.be>
2024-12-24 10:57:03 +0100

    Made Manager actually a Person variant

--- src/main/java/com/switchfully/parkshark_2024_10/user/Manager.java
+++ src/main/java/com/switchfully/parkshark_2024_10/user/Manager.java
@@ -1,8 +1,12 @@
 package com.switchfully.parkshark_2024_10.user;
 
 import com.switchfully.parkshark_2024_10.auth.Role;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
-public class Manager extends Person{
+@Entity
+@DiscriminatorValue("2")
+public class Manager extends Person {
     public Manager(String firstName, String lastName, String email, String password) {
         super(firstName, lastName, email, password);
     }
@@ -14,6 +18,4 @@ public class Manager extends Person{
     public Role getRole() {
         return Role.MANAGER;
     }
-
-
 }
